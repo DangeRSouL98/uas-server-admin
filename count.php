54a7d05f3dbc8c24ad06b4d77ebc0138292c2852
DangeRSouL98 <wellsonleewando0908@gmail.com>
2022-02-14 14:02:38 -0800

    Add new file

---
+++ count.php
@@ -0,0 +1,13 @@
+<?php
+    $conn = new mysqli("192.168.79.128", "root", "1234", "trucorp-web-2.0");
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $result = $conn->prepare('SELECT ID, Nama, Alamat, Jabatan FROM users');
+    $result->execute();
+    echo $result->get_result()->num_rows;
+
+    $conn->close();
+?>
